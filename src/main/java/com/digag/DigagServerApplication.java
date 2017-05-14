261a82bf4d44ff54310e8a2a046430404527445b
Yuicon <910722178@qq.com>
2017-05-14 17:46:53 +0800

    a

---
+++ src/main/java/com/digag/DigagServerApplication.java
@@ -0,0 +1,12 @@
+package com.digag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigagServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigagServerApplication.class, args);
+	}
+}
