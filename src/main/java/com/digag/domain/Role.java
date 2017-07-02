516522fa69b5bbaaa35cb6a2dfcce0515b773eea
Yuicon <910722178@qq.com>
2017-07-02 15:35:43 +0800

    perf:修改登陆接口，返回信息更加友好

--- src/main/java/com/digag/domain/Role.java
+++ src/main/java/com/digag/domain/Role.java
@@ -55,4 +55,13 @@ public class Role {
     public void setUsers(List<User> users) {
         this.users = users;
     }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
 }
