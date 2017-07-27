681ab84c09c5b7f663ded725ed018d778b1f9f86
Yuicon <910722178@qq.com>
2017-07-27 23:37:25 +0800

    perf:缓存条目列表

--- src/main/java/com/digag/domain/Repository/EntryRepository.java
+++ src/main/java/com/digag/domain/Repository/EntryRepository.java
@@ -1,13 +1,26 @@
 package com.digag.domain.Repository;
 
 import com.digag.domain.Entry;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+import static javafx.scene.input.KeyCode.T;
 
 /**
  * Created by Yuicon on 2017/7/8.
  * https://github.com/Yuicon
  */
 @RepositoryRestResource
+@CacheConfig(cacheNames = "entries")
 public interface EntryRepository extends JpaRepository<Entry,String> {
+
+    @Cacheable()
+    Page<Entry> findAll(Pageable var1);
+
 }
--- src/main/java/com/digag/service/Impl/EntryServiceImpl.java
+++ src/main/java/com/digag/service/Impl/EntryServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.*;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
